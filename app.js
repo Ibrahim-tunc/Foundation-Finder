e1278f27a58ba0ff6a27333fdee1b369c690a5b9
Ibrahim-tunc <46247396+Ibrahim-tunc@users.noreply.github.com>
2023-11-26 23:25:29 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,485 @@
+const config = {
+    desktopWidth: "1200px",
+    mobileWidth: "320px",
+};
+
+const data = [
+    {
+        attribute: 
+        {
+            "Hafif": 
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103334262.jpg",
+                value: "LIGHT",
+                disabled: false,
+                attr: "attribute6"
+            },
+            "Orta":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103430061.jpg",
+                value: "MIDDLE",
+                disabled: false,
+                attr: "attribute6"
+            },
+            "Yüksek":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103457343.jpg",
+                value: "HIGH",
+                disabled: false,
+                attr: "attribute6"
+            }
+        },
+        title: "Nasıl bir kapatıcılık hayal ediyorsunuz?",
+        textTerm: ["Hangi kapatıcılık benim için en iyisi? " , "Hangi kapsam benim için en iyisidir?", "<strong>Hafif kapatıcılık:</strong> Cildinizde kusur yok veya doğal bir etki istiyorsunuz.<br><strong> Orta kapsam:</strong> cildinizde küçük kusurlar veya renk değişimleri var.<br> <strong>Yüksek kapatıcılık:</strong> Cildinizde bariz kusurlar var."]
+    },
+    {
+        attribute: 
+        {
+            "Doğal": 
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103525482.jpg",
+                value: "NATURAL",
+                disabled: false,
+                attr: "attribute7"
+            },
+            "Parlak":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103615201.jpg",
+                value: "METALLIC",
+                disabled: false,
+                attr: "attribute7"
+            },
+            "Mat":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103550172.jpg",
+                value: "MATTE",
+                disabled: false,
+                attr: "attribute7"
+            }
+        },
+        title: "Nasıl bir bitiş hayal ediyorsunuz?",
+        textTerm: null
+    },
+    {
+        attribute: 
+        {
+                "Kuru": 
+                {
+                    src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103659638.jpg",
+                    value: "DRY",
+                    disabled: false,
+                    attr: "attribute8"
+                },
+                "Yağlı":
+                {
+                    src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103716535.jpg",
+                    value: "OILY",
+                    disabled: false,
+                    attr: "attribute8"
+                },
+                "Karma":
+                {
+                    src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103638847.jpg",
+                    value: "COMBINATION",
+                    disabled: false,
+                    attr: "attribute8"
+                },
+                "Normal":
+                {
+                    src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103736207.jpg",
+                    value: "NORMAL",
+                    disabled: false,
+                    attr: "attribute8"
+                }
+        },
+        title: "Nasıl bir cilt tipine sahipsiniz?",
+        textTerm: ["Cilt tipimi nasıl belirleyebilirim? ", "Cilt tipimi nasıl belirleyebilirim? ", "<strong>Kuru:</strong> genellikle gergin ve pürüzlü, mat görünür ve hidratlandığında bile çok elastik değildir. <br><strong>Yağlı:</strong> Parlak bir görünüme sahiptir, gözenekler genişlemiş ve net bir şekilde görünür durumdadır ve sivilceye eğilimlidir. <br><strong>Kombinasyon:</strong> yanaklar normal veya bazen kuruyken, safsızlıklar içeren yağlı bir T bölgesi ile karakterizedir. <br><strong>Normal:</strong> İyi dengelenmiş, pürüzsüz ve belirli kusurları yok."]
+    },
+    {
+        attribute: 
+        {
+            "Çok Açık": 
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103849881.jpg",
+                value: "LIGHT",
+                disabled: false,
+                attr: "attribute9"
+            },
+            "Açık":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103814751.jpg",
+                value: "LIGHT-MEDIUM",
+                disabled: false,
+                attr: "attribute9"
+            },
+            "Orta":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103833263.jpg",
+                value: "MEDIUM",
+                disabled: false,
+                attr: "attribute9"
+            },
+            "Koyu":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103756317.jpg",
+                value: "MEDIUM-DARK",
+                disabled: false,
+                attr: "attribute9"
+            },
+            "Çok Koyu":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103906400.jpg",
+                value: "DARK",
+                disabled: false,
+                attr: "attribute9"
+            }
+        },
+        title: "Nasıl bir cilt tonuna sahipsiniz?",
+        textTerm: null
+    },
+    {
+        attribute: 
+        {
+            "Sıcak": 
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810104005038.jpg",
+                value: "GOLD",
+                disabled: false,
+                attr: "attribute10"
+            },
+            "Nötr":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810104023070.jpg",
+                value: "NEUTRAL",
+                disabled: false,
+                attr: "attribute10"
+            },
+            "Soğuk":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103928697.jpg",
+                value: "ROSE",
+                disabled: false,
+                attr: "attribute10"
+            },
+            "Bilmiyorum":
+            {
+                src: "https://imgvisilabsnet.azureedge.net/banner/uploaded_images/418_1455_20230810103946516.jpg",
+                value: null,
+                disabled: false,
+                attr: "attribute10"
+            }
+        },
+        title: "Nasıl bir cilt alt tonuna sahipsiniz?",
+        textTerm: ["Alt tonumu nasıl bulurum?", "Alt tonumu nasıl bulurum?", "Cildinizi makyajsız ve doğal güneş ışığında görün. <br><strong>Sıcak:</strong> Cilt tonunuz sarıya doğru eğilimlidir. Kolayca bronzlaşırsınız ve damarlarınız mavi-yeşil görünür. Altın takılar üzerinizde öne çıkıyor.<br><strong>Nötr:</strong> Cildiniz pembe ve sarı arasında dengelenmiştir. Bileğin damarları hem mavi-mor hem de mavi-yeşildir. Güneşte yanarsın ama sonra bronzlaşmaya başlarsın.<br><strong>Soğuk:</strong> Cildiniz pembeleşmeye eğilimlidir. Güneşte bronzlaşmaz, ancak güneş kremi sürmezseniz yanma eğilimi gösterir (bunu asla unutmayın!). Damarlarınız mavi veya mor."]
+    },
+];
+
+
+FireFinder();
+function FireFinder() {
+    if (!document.querySelector("#vl-popup-container")) {
+        var style = document.createElement("style");
+        style.innerHTML = `
+        #vl-popup-container {
+            z-index: 10000;
+            width: 100%;
+            height: 100%;
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+        
+        #vl-popup-container .vl-popup-overlay {
+            z-index: 10000;
+            width: 100%;
+            height: 100%;
+            background: black;
+            opacity: 0.6;
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+        
+        #vl-popup-container .vl-popup {
+            position: absolute;
+            z-index: 100001;
+            width: ${config.desktopWidth};
+            height: 600px;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            -webkit-transform: translate(-50%, -50%);
+            background-color: #F0F0F0;
+            display: flex;
+            padding: 3em;
+            justify-content: center;
+        }
+        
+        
+        #vl-popup-container .vl-popup-close {
+            position: absolute;
+            right: 1em;
+            top: 1em;
+            cursor: pointer;
+            z-index: 2;
+            width: 2em;
+            height: 2em;
+            background-color: white;
+            color: black;
+            display: grid;
+            place-items: center;
+            border-radius: 50%;
+        }
+
+        .vl-back{
+            position: absolute;
+            left: 1em;
+            top: 1em;
+            cursor: pointer;
+            z-index: 2;
+            height: 2em;
+            text-align: center;
+            background-color: white;
+            color: black;
+            display: grid;
+            place-items: center;
+            padding: 0 1em;
+            font-weight: 600;
+        }
+
+        .vl-page{
+            display: flex;
+            gap: 3em;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 2em;
+        }
+
+        .vl-page h1{
+            font-size: 1.4em;
+            font-weight: 700;
+        }
+
+        .vl-page-layout{
+            display: flex;
+            gap: 1em;
+        }
+
+        .vl-page-count{
+            font-size: 1.4em;
+            font-weight: 800;
+        }
+
+        .vl-container-icon{
+            width: 200px;
+            height: 80px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background: white;
+            gap: 5px;
+            text-transform: uppercase;
+            font-size: 1.2em;
+            font-weight: 700;
+            border-radius: 9px;
+            cursor: pointer;
+        }
+
+        .vl-mini-popup{
+            position: absolute;
+            width: 350px;
+            background-color: white;
+            display: none;
+            padding: 1em;
+            z-index: 100002;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            -webkit-transform: translate(-50%, -50%);
+            flex-direction: column;
+            gap: 1em;
+            align-items: center;
+            text-align: center;    
+            border-radius: 1em;
+        }
+
+        .vl-mini-popup span:first-child{
+            font-size: 1.2em;
+            font-weight: 700;
+        }
+
+        .vl-mini-popup p{
+            font-size: .87em;
+        }
+
+        .vl-mini-btn{
+            color: #D11A6F;
+            font-size: 1.1em;
+            font-weight: 600;
+            cursor: pointer;
+        }
+
+        .vl-mini-close{
+            background-color: #D11A6F;
+            color: white;
+            padding: 0.2em 0.5em;
+            border-radius: 2px;
+            text-align: center;
+            cursor: pointer;
+        }
+
+        .vl-container-icon img{
+            width: 100%;
+            border-radius: 9px 9px 0 0;
+        }
+
+        .vl-disabled-true{
+            opacity: .5;
+            pointer-events: none;
+        }
+        
+        @media only screen and (max-width: 768px) {
+            #vl-popup-container .vl-popup {
+                width:${config.mobileWidth};
+            }
+        
+            #vl-popup-container .vl-popup-image {
+                width:${config.mobileWidth};
+            }
+        }
+        `;
+        document.head.append(style);
+
+        const answers = [];
+        let count = -1;
+
+        const popup = document.createElement("div");
+        popup.id = "vl-popup-container";
+        popup.innerHTML = `
+        <div class="vl-popup-overlay"></div> 
+        <div class="vl-popup">
+          <span class="vl-popup-close">&#x2716;</span>
+          <div class="vl-page">
+            <span class="vl-page-count">${count+2} / 5</span>
+            <h1 class="vl-page-header">${data[0].title}</h1>
+            <div class="vl-page-layout">${page()}</div>
+          </div>
+        </div>
+        `;
+        document.body.append(popup);
+        popupEvents();
+        miniPopup(data[count].textTerm);
+        event();
+        
+
+        function event(){
+            Object.keys(data[count].attribute).forEach(target => {
+                disabledButtons([...answers, {attribute: data[count].attribute[target].attr, value: data[count].attribute[target].value}], data[count].attribute[target].value, count);
+            });
+
+            popup.querySelectorAll('.vl-container-icon').forEach(icon => {
+                icon.addEventListener('click', () => {
+                    answers.push({attribute: icon.querySelector('img').dataset.attr, value: icon.querySelector('img').alt});
+                    if(count < 4){
+                        creator();
+                    }
+                    else{
+                        console.log(answers);
+                    }
+                });
+            });
+
+            popup.querySelector('.vl-back')?.addEventListener('click', () => {
+                count = count-2;
+                answers.pop();
+                creator();
+            });
+        }
+        
+        function creator(){
+            popup.querySelector('.vl-popup').innerHTML = 
+            `
+                <span class="vl-popup-close">&#x2716;</span>
+                <div class="vl-page">
+                    <span class="vl-page-count">${count+2} / 5</span>
+                    <h1 class="vl-page-header">${data[count+1].title}</h1>
+                    <div class="vl-page-layout"> ${page()} </div>
+                </div>
+            `;
+            if(count > -1){
+                popup.querySelector('.vl-popup').innerHTML += "<span class='vl-back'>Geri</span>";
+            } 
+            miniPopup(data[count].textTerm);
+            popupEvents();
+            event();
+        }
+
+        function page(){
+            count++;
+            const attributes = data[count].attribute;
+            const innerKeys = Object.keys(attributes);
+            return innerKeys.map(key => 
+                icon(key, attributes[key].src, attributes[key].value, attributes[key].attr, attributes[key].disabled)
+            ).join("");  
+        }
+
+        function icon(key, imgSrc, value, attr, disabled){
+            return `
+                    <div class="vl-container-icon vl-disabled-${disabled}">
+                        <img data-attrtr="${key}" data-attr="${attr}" alt="${value}" src=${imgSrc}>
+                        <span>${key}</span>
+                    </div>
+                    `;
+        }
+
+        function miniPopup(term){
+            if(term != null){
+                popup.querySelector('.vl-page').innerHTML += `<span class="vl-mini-btn">${term[0]}</span>`;
+                popup.querySelector('.vl-popup').innerHTML += 
+                `
+                    <div class="vl-mini-popup">
+                        <span>${term[1]}</span>
+                        <p>${term[2]}</p>
+                        <span class="vl-mini-close">Anladım.</span>
+                    </div>
+                `;
+                popup.querySelector('.vl-mini-btn').addEventListener('click', () => {
+                    popup.querySelector('.vl-mini-popup').style.display = "flex";
+                });
+                popup.querySelector('.vl-mini-close').addEventListener('click', () => {
+                    popup.querySelector('.vl-mini-popup').style.display = "none";
+                });
+            }
+        }
+
+        function disabledButtons(dataset, target, count) {
+            const filter = [];
+            dataset.forEach(data => {
+                filter.push(new VL_OfferFilter(data.attribute, VL_OfferFilterType.Equals, data.value));
+            });
+            isExist(filter, target, count);
+        }
+
+        function isExist(filter, target, count){
+            const exist = false;
+            var vl = new Visilabs();
+            vl.AddParameter("json",true);
+            vl.Suggest(62, null, null, CallBackMethod, null, filter);
+            function CallBackMethod(products){
+                if(products < 1){
+                    popup.querySelector(`img[alt="${target}"]`).parentElement.classList.add('vl-disabled-true');
+                }
+            }
+        }
+
+        function popupEvents(){
+            popup.querySelector(".vl-popup-overlay").addEventListener("click", closePopup);
+            popup.querySelector(".vl-popup-close").addEventListener("click", closePopup);
+        }
+
+        function closePopup() {
+            popup.remove();
+            style.remove();
+        }
+    }
+}
